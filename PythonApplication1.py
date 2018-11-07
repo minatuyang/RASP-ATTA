391277d44162e089119d4cd5b349502b86e729a6
haodada <haodadada140328@gmail.com>
2018-11-07 14:27:06 +0800

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -40,9 +40,69 @@ class CaptureManager(object):
     def isWritingVideo(self):
         return self._videoFilename is not None
 
+    def enterFrame(self):
+        """Capture the next frame,if any."""
+        # assert not self._enteredFrame, \'previous'
+        if self._capture is not None:
+            self._enteredFrame = self._capture.grab()
+
+    def exitFrame(self):
+        if self.frame is None:
+            self._enteredFrame = False
+            return
+        if self._framesElapsed == 0:
+            self._startTime = time.time()
+        else:
+            timeElapsed = time.time() - self._startTime
+        self._fpsEstimate = self._framesElapsed / timeElapsed
+
+        self._framesElapsed += 1
+        if self.previewWindowManager is not None:
+            if self.shouldMirrorPreview:
+                mirroredFrame = np.fliplr(self._frame).copy()
+                self.previewWindowManager.show(mirroredFrame)
+            else:
+                self.previewWindowManager.show(self._frame)
+        if self.isWritingImage:
+            cv2.imwrite(self._imageFilename, self._frame)
+            self._imageFilename = None
+        self._writeVideoFrame()
+        self._frame = None
+        self._enteredFrame = False
+
+    def writeImage(self, filename):
+        self._imageFilename = filename
+
+    def startWritingVideo(self, filename, encoding=cv2.VideoWriter_fourcc('I', '4', '2', '0')):
+        self._videoFilename = filename
+        self._videoEncoding = encoding
+
+    def stopWritingVideo(self):
+        self._videoFilename = None
+        self._videoEncoding = None
+        self._videoWriter = None
+
+    def _writeVideoFrame(self):
+        if not self.isWritingVideo:
+            return
+        if self._videoWriter is None:
+            fps = self._capture.get(cv2.CAP_PROP_FPS)
+            if fps == 0.0:
+                if self._framesElapsed < 20:
+                    return
+                else:
+                    fps = self._fpsEstimate
+            size = (int(self._capture.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                    int(self._capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            self._videoWriter = cv2.VideoWriter(
+                self._videoFilename, self._videoEncoding, fps, size)
+            self._videoWriter.write(self._frame)
+
 
 cameraCapture = cv2.VideoCapture(0)
 ret = True
+a = CaptureManager(1)
+print(a.isWritingImage)
 while ret:
     ret, frame = cameraCapture.read()
     # frame=cv2.flip(frame,0)#上下翻转
