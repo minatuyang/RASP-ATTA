fd225d8c05d9b212fc27a06bae40429c6a438182
haodada <haodadada140328@gmail.com>
2018-11-16 11:01:21 +0800

    Update servo_sample.py

--- servo_control/servo_sample.py
+++ servo_control/servo_sample.py
@@ -3,6 +3,8 @@ import time
 import signal
 import atexit
 import numpy as np
+GPIO.setwarnings(False)
+GPIO.cleanup
 atexit.register(GPIO.cleanup)  # 在程序退出时执行GPIO指令清空
 GPIO.setmode(GPIO.BCM)  # 设置GPIO的编号格式为BCM
 # BCM/BOARD
@@ -11,20 +13,24 @@ GPIO.setmode(GPIO.BCM)  # 设置GPIO的编号格式为BCM
 GPIO.setup(15, GPIO.OUT, initial=False)  # 设置GPIO为推挽模式
 GPIO.setup(18, GPIO.OUT, initial=False)
 p1 = GPIO.PWM(15, 50)  # 设置GPIO为50Hz的PWM输出
-p2 = GPIO.PWM(15, 50)  # 50HZ
+p2 = GPIO.PWM(18, 50)  # 50HZ
 
 p1.start(0)  # 开始PWM输出，输出占空比为0
 p2.start(0)
 
-time.sleep(2)
+time.sleep(1)
 
 while True:
-    p.ChangeDutyCycle(12.5)  # 设置转动角度至180
+    p1.ChangeDutyCycle(12.5)  # 设置转动角度至180
+    p2.ChangeDutyCycle(12.5)  # 设置转动角度至180
     time.sleep(0.2)
-    p.ChangeDutyCycle(0)  # 保持当前角度
-    time.sleep(5)
+    p1.ChangeDutyCycle(0)  # 保持当前角度
+    p2.ChangeDutyCycle(0)  # 保持当前角度
+    time.sleep(2)
 
-    p.ChangeDutyCycle(2.5)  # 设置转动角度至0
+    p1.ChangeDutyCycle(2.5)  # 设置转动角度至0
+    p2.ChangeDutyCycle(2.5)  # 设置转动角度至0
     time.sleep(0.2)
-    p.ChangeDutyCycle(0)  # 保持当前角度
-    time.sleep(5)
+    p1.ChangeDutyCycle(0)  # 保持当前角度
+    p2.ChangeDutyCycle(0)  # 保持当前角度
+    time.sleep(2)
